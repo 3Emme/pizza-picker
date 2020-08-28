9a93ddcfba0435437de1772746a4d9c4fbf71e3c
3Emme <emmettbuentiempo@gmail.com>
2020-08-28 15:37:24 -0700

    create first pass at pizzaToppingArray function and alter form submit function to better fit the new model.

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,16 @@ let orderNumber = 0
     event.preventDefault();
     // const pizza1Name = $("input#pizza1NameInput").val();
     // console.log("pizza1Name: " + pizza1Name);
-    let pizzaToppings = pizzaToppingArray (values passed from form);
-    let pizzaSize = 
+    let pizzaToppingInputs = $("input:checkbox[name=pizza_toppings]:checked");
+    console.log("pizzaToppingInputs: " + pizzaToppingInputs);
+    let pizzaToppings = pizzaToppingArray(pizzaToppingInputs);
+    let pizzaSize = $("input:radio[name=pizza_size]:checked")
+    $("input:checkbox[name=pizza_toppings]:checked").each(function(){
+      const pizzaTopping = $(this).val();
+      const pizzaToppings = []];
+      
+      return pizzaToppings;
+    });
     pizza1 = new Pizza (orderNumber, pizzaToppings, pizzaSize);   // Globally set variable pizza1
     orderNumber += 1;
     $("#pizza1NameOutput").text(pizza1Name);
@@ -30,31 +38,40 @@ let orderNumber = 0
     $("#pizzaNameInputForm").trigger("reset");
   });
 
-  function pizzaToppingArray (values passed from form){
+  function pizzaToppingArray (pizzaToppingInputs){
     let pizzaToppings = []
-
+    pizzaToppingInputs.each(function(){
+      const pizzaToppings = $(this).val();
+      pizzaToppings.push();
+    }
+  return pizzaToppings;
   }
-
-
 });
 
 // User Interface Logic End
 // index.html Key:
 
 // form-pizza: id="pizza_picker_form"
-// form-topping-redSauce: name="pizza_topping_redSauce" value="redSauce" id="pizza_topping_redSauce"
-// form-topping-cheese: name="pizza_topping_cheese" value="cheese" id="pizza_topping_cheese"
-// form-topping-pepperoni: name="pizza_topping_pepperoni" value="pepperoni" id="pizza_topping_pepperoni"
-// form-topping-mushrooms: name="pizza_topping_mushrooms" value="mushrooms" id="pizza_topping_mushrooms"
-// form-topping-onions: name="pizza_topping_onions" value="onions" id="pizza_topping_onions"
-// form-topping-sausage: name="pizza_topping_sausage" value="sausage" id="pizza_topping_sausage"
-// form-topping-bacon: name="pizza_topping_bacon" value="bacon" id="pizza_topping_bacon"
-// form-topping-blackOlives: name="pizza_topping_blackOlives" value="blackOlives" id="pizza_topping_blackOlives"
-// form-topping-greenPeppers: name="pizza_topping_greenPeppers" value="greenPeppers" id="pizza_topping_greenPeppers"
-// form-topping-pineapple: name="pizza_topping_pineapple" value="pineapple" id="pizza_topping_pineapple"
-// form-size-null: name="pizza_picker_size" id="pizza_size_null" value=""
-// form-size-small: name="pizza_picker_size" id="pizza_size_small" value="small"
-// form-size-medium: name="pizza_picker_size" id="pizza_size_medium" value="medium"
-// form-size-large: name="pizza_picker_size" id="pizza_size_large" value="large"
-// form-size-extraLarge: name="pizza_picker_size" id="pizza_size_extraLarge" value="extraLarge"
+// form-topping-redSauce: name="pizza_toppings" value="redSauce" id="pizza_topping_redSauce"
+// form-topping-cheese: name="pizza_toppings" value="cheese" id="pizza_topping_cheese"
+// form-topping-pepperoni: name="pizza_toppings" value="pepperoni" id="pizza_topping_pepperoni"
+// form-topping-mushrooms: name="pizza_toppings" value="mushrooms" id="pizza_topping_mushrooms"
+// form-topping-onions: name="pizza_toppings" value="onions" id="pizza_topping_onions"
+// form-topping-sausage: name="pizza_toppings" value="sausage" id="pizza_topping_sausage"
+// form-topping-bacon: name="pizza_toppings" value="bacon" id="pizza_topping_bacon"
+// form-topping-blackOlives: name="pizza_toppings" value="blackOlives" id="pizza_topping_blackOlives"
+// form-topping-greenPeppers: name="pizza_toppings" value="greenPeppers" id="pizza_topping_greenPeppers"
+// form-topping-pineapple: name="pizza_toppings" value="pineapple" id="pizza_topping_pineapple"
+// form-size-null: name="pizza_size" id="pizza_size_null" value=""
+// form-size-small: name="pizza_size" id="pizza_size_small" value="small"
+// form-size-medium: name="pizza_size" id="pizza_size_medium" value="medium"
+// form-size-large: name="pizza_size" id="pizza_size_large" value="large"
+// form-size-extraLarge: name="pizza_size" id="pizza_size_extraLarge" value="extraLarge"
 // form-button-submit: id="pizza_picker_form_submit_button"
+
+
+
+//     $("input:checkbox[name=pizza_toppings]:checked").each(function(){
+//   const pizzaToppings = $(this).val();
+//   $('#work-responses').append(workTransportationMode + "<br>");
+// });
