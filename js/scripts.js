d681cfd4b71e517178b28e161c93af4d85c5f886
3Emme <emmettbuentiempo@gmail.com>
2020-08-28 11:04:15 -0700

    add initial comment structure for business and user interface logic. add constructor function for a pizza.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Business Logic Start
+//Constructor function for a pizza
+function Pizza(customerName,orderNumber) {
+  this.customerName = customerName;
+  this.orderNumber = orderNumber;
+  this.toppings = [];
+  this.size = "";
+}
+//Business Logic End
+//User Interface Logic Start
+$(document).ready(function() {
+
+});
+//User Interface Logic End
